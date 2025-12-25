f52d9cfbb1b7f6bf839709acb4b5435401d038c2
Dianamikhailova <mikhayl-dia@mail.ru>
2025-12-25 14:18:15 +0300

    First commit

---
+++ script/DeployGame.s.sol
@@ -0,0 +1,15 @@
+// script/DeployGame.s.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {GuessNumberGame} from "../src/GuessNumberGame.sol";
+
+contract DeployGame is Script {
+    function run() external {
+        vm.startBroadcast();
+        new GuessNumberGame();
+        vm.stopBroadcast();
+    }
+}
+
